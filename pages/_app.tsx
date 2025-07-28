ce8ab073eeeed43f81087ba7e81342770e80541c
Sunkan111 <Filip.sunden2@gmail.com>
2025-07-29 01:18:13 +0200

    Update _app.tsx to wrap AuthProvider and CartProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { AuthProvider } from '../contexts/AuthContext';
+import { CartProvider } from '../contexts/CartContext';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
+    </AuthProvider>
+  );
 }
